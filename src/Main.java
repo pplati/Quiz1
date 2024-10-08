678a08416e53f891d3fb13ba24afa86c9c8ab5f6
pplati <plati.panagiota@gmail.com>
2024-10-08 16:34:31 +0300

    nees plirofories

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,19 @@ public class Main {
 
         Customer customer = new Customer();
         customer.setAddress("Athens");
+        customer.setSurname("Plati");
+
+        Product product = new Product();
+        product.setPrice(20.00);
+        product.setName("chips");
+
+        double fpa = 0.24;
+        Payment payment = new Payment();
+        payment.setCustomer(customer);
+        payment.setAmount(product.getPrice()*(1+fpa));
+
+        System.out.println(payment.getAmount());
+
         Customer customer2 = new Customer();
         customer2.setAddress("Berlin");
         System.out.println(customer.getAddress());
